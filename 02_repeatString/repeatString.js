823c317ce80f91b9dd4e6b7b985d7194ab8a83ea
roxayp <roxanne.proulx87@gmail.com>
2023-02-15 14:46:31 -0500

    tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
+// create a function to repeat a string a number of times
+// repeatString('hey', 3) should return 'heyheyhey'
+// repeatString('hey', 0) should return ''
+// repeatString('hey', -1) should return 'ERROR'
 
-};
+const repeatString = function(x, y) {
+    let string = '';
+    if (y < 0) {
+        return 'ERROR';
+    } else {
+        for (let i = 0; i < y; i++) {
+            string += x;
+        }
+    }
+    return string;
+}
+
 
 // Do not edit below this line
 module.exports = repeatString;
