1bfe68e8afd9f70eaea5322c7850941de0aae6a8
roxayp <roxanne.proulx87@gmail.com>
2023-02-15 14:52:45 -0500

    test passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,7 @@
-const reverseString = function() {
+const reverseString = (str) => {
+    return str.split('').reverse().join('');
+}
 
-};
 
 // Do not edit below this line
 module.exports = reverseString;
